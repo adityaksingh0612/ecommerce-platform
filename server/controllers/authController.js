1e3b3c9d0688337a588ba6e28bb2e92de8885755
Aditya <aditya@js-MacBook-Air.local>
2026-07-12 18:27:15 +0530

    Add home page and product listing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -57,8 +57,14 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   res.status(200).json({
-    message: "Login successful",
-    token,
+  message: "Login successful",
+  token,
+  user: {
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  },
   });
 });
 
